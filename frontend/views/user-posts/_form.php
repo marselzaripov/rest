aa7d15ce9a23b93175628a2f5713a04437faebd5
Марсель <marsel-zaripov-1988@mail.ru>
2019-04-04 13:38:51 +0300

    ckeditor

--- frontend/views/user-posts/_form.php
+++ frontend/views/user-posts/_form.php
@@ -2,11 +2,18 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
+use dosamigos\ckeditor\CKEditor;
+use dosamigos\ckeditor\CKEditorInline;
 /* @var $this yii\web\View */
 /* @var $model common\models\Post */
 /* @var $form yii\widgets\ActiveForm */
 ?>
+
+
+
+
+
+
 
 <div class="post-form">
 
@@ -14,11 +21,20 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
 
-    <?/*= Html::a('Set Image', ['set-image', 'id' => $model->id], ['class' => 'btn btn-default']) */?>
 
-    <a href="/posts/set-image?id=1" class="btn btn-default">Set Image</a>
+    <?= $form->field($model, 'content')->widget(CKEditor::className(), [
+        'options' => ['rows' => 6],
+        'preset' => 'full',
+
+
+    ]) ?>
+
+    <?/*= $form->field($model, 'content')->textarea(['rows' => 6]) */?>
+
+    <?/*= Html::a('Set Image', ['set-image', 'id' => $model->id], ['class' => 'btn btn-default'])*/ ?>
+
+   <a href="/posts/set-image?id=<?=$model->id;?>" class="btn btn-default">Set Image</a>
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
